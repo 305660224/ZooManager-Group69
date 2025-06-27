0c9f777f5c21d261b8089e292164f17ad787f3bf
JairoLG <lagartijayt1822@gmail.com>
2025-06-27 14:21:43 -0600

    Resolucion conflicto en list
    Se agregan los metodos add y remove

--- src/Lists/List.java
+++ src/Lists/List.java
@@ -1,14 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+<<<<<<< HEAD
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+=======
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+>>>>>>> e335df4 (Clase List)
  */
 package Lists;
 
 /**
  *
+<<<<<<< HEAD
  * @author ASUS
  */
 public interface List <T>{
     public boolean add(T t);
-    
+    public boolean remove(T t);
 }
